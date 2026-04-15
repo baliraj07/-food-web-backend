eb58b57e0a01e5904bf0b4858ca155aa06c81791
surajugale12 <surajugale1453@gmail.com>
2026-04-16 02:00:49 +0530

    final fix restaurant controller

--- routes/restaurants.js
+++ routes/restaurants.js
@@ -1,11 +1,16 @@
 const express = require('express');
+const { getAllRestaurants, getRestaurantById, createRestaurant, updateRestaurant, deleteRestaurant } = require('../controllers/restaurantController');
+
+const { auth, isOwner } = require('../middleware/auth');
+
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  res.json([
-    { name: "Test Restaurant 1" },
-    { name: "Test Restaurant 2" }
-  ]);
-});
+router.get('/', getAllRestaurants);
+router.get('/:id', getRestaurantById);
+router.post('/', auth, createRestaurant);
+router.put('/:id', auth, isOwner, updateRestaurant);
+router.delete('/:id', auth, isOwner, deleteRestaurant);
+
 
 module.exports = router;
+console.log("SERVER STARTED");
--- server.js
+++ server.js
@@ -38,4 +38,4 @@ app.get('/', (req, res) => {
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
-console.log("RESTAURANTS ROUTE LOADED");
+console.log("RESTAURANTS ROUTE LOADED");  
